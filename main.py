c5568be64a9bb8243485fa4b7a36a9ccebbbb070
theliteprogrammer <219037801@student.uj.ac.za>
2023-04-28 10:08:27 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+
+if __name__ == '__main__':
+    # name of person who will be asking the Magic 8-Ball
+    name = "Keanu Reeves"
+    question = "Will I win the lottery?"
+    answer = ""
+    random_number = random.randint(1, 9)
+    # print(random_number)
+    if random_number == 1:
+        answer = "Yes - definitely"
+    elif random_number == 2:
+        answer = "It is decidedly so"
+    elif random_number == 3:
+        answer = "Without a doubt"
+    elif random_number == 4:
+        answer = "Reply hazy, try again"
+    elif random_number == 5:
+        answer = "Ask again later"
+    elif random_number == 6:
+        answer = "Better not tell you now"
+    elif random_number == 7:
+        answer = "My sources say so"
+    elif random_number == 8:
+        answer = "Outlook not so good"
+    elif random_number == 9:
+        answer = "Very doubtful"
+    else:
+        answer = "Error"
+    if question == "":
+        print("No question was asked")
+    else:
+        if name == "":
+            print("Question: " + question)
+        else:
+            print(name + " asks: " + question)
+    print("Magic 8-Ball's answer: " + answer)
+
+
+
